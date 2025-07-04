6b067fb869c801e5c6b4ef4ca659eb6b6c02b0de
gibran.meidiyan <mochamad.meidiyan@spesolution.com>
2025-07-04 20:27:44 +0700

    v

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -128,7 +130,9 @@ func processSRWebhook(cfg Config, db, mwdb *sql.DB, w http.ResponseWriter, r *ht
 		PatientName      string `json:"patient_name"`
 		StudyInstanceUID string `json:"study"`
 	}
-	log.Printf("Menerima webhook r.Body: %s", r.Body)
+	bodyBytes, _ := io.ReadAll(r.Body)
+	log.Printf("Menerima webhook r.Body: %s", string(bodyBytes))
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // agar bisa di-decode lagi di bawah
 	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
 		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
 		SavePortalLog(mwdb, "[SR] Webhook gagal: payload tidak valid")
