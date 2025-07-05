f9640935a41ee4a26e70214c4f23e82ee4a31040
gibran.meidiyan <mochamad.meidiyan@spesolution.com>
2025-07-05 21:32:30 +0700

    asf

--- khanza.go
+++ khanza.go
@@ -47,7 +47,7 @@ func ConnectKhanzaDB(cfg Config) (*sql.DB, error) {
 
 func GetPendingWorklist(db *sql.DB, tglPermintaan string) ([]WorklistRequest, error) {
 	query := `SELECT
-    p.no_rkm_medis AS PatientID,
+    pr.noorder AS PatientID,
     p.nm_pasien AS PatientName,
     DATE_FORMAT(p.tgl_lahir, '%Y%m%d') AS PatientBirthDate,
     CASE p.jk
@@ -55,17 +55,30 @@ func GetPendingWorklist(db *sql.DB, tglPermintaan string) ([]WorklistRequest, er
         WHEN 'P' THEN 'F'
         ELSE 'O'
     END AS PatientSex,
-    pr.noorder AS AccessionNumber,
+    p.no_rkm_medis AS AccessionNumber,
 	IFNULL(pj.kd_jenis_prw, '') AS RequestedProcedureID,
 	IFNULL(jpr.nm_perawatan, '') AS RequestedProcedureDescription,
     CONCAT(pr.noorder, DATE_FORMAT(pr.tgl_permintaan, '%Y%m%d'), REPLACE(pr.jam_permintaan, ':', '')) AS ScheduledProcedureStepID,
     DATE_FORMAT(pr.tgl_permintaan, '%Y%m%d') AS ScheduledProcedureStepStartDate,
     REPLACE(pr.jam_permintaan, ':', '') AS ScheduledProcedureStepStartTime,
-    CASE
-        WHEN jpr.nm_perawatan LIKE '%Rontgen%' THEN 'CR'
-        WHEN jpr.nm_perawatan LIKE '%USG%' THEN 'US'
-        WHEN jpr.nm_perawatan LIKE '%CT%' THEN 'CT'
-        ELSE 'OT'
+	CASE
+    WHEN jpr.nm_perawatan LIKE '%CR%' OR jpr.nm_perawatan LIKE '%Rontgen%' OR jpr.nm_perawatan LIKE '%Computed Radiography%' THEN 'CR'
+    WHEN jpr.nm_perawatan LIKE '%CT%' OR jpr.nm_perawatan LIKE '%CT Scan%' THEN 'CT'
+    WHEN jpr.nm_perawatan LIKE '%DX%' OR jpr.nm_perawatan LIKE '%General Radiography%' THEN 'DX'
+    WHEN jpr.nm_perawatan LIKE '%ECG%' OR jpr.nm_perawatan LIKE '%Elektrokardiogram%' OR jpr.nm_perawatan LIKE '%Electrocardiogram%' THEN 'ECG'
+    WHEN jpr.nm_perawatan LIKE '%EPS%' OR jpr.nm_perawatan LIKE '%Electrophysiology%' THEN 'EPS'
+    WHEN jpr.nm_perawatan LIKE '%ES%' OR jpr.nm_perawatan LIKE '%Endoscopy%' THEN 'ES'
+    WHEN jpr.nm_perawatan LIKE '%MG%' OR jpr.nm_perawatan LIKE '%Mammo%' OR jpr.nm_perawatan LIKE '%Mammografi%' OR jpr.nm_perawatan LIKE '%Mammography%' THEN 'MG'
+    WHEN jpr.nm_perawatan LIKE '%MR%' OR jpr.nm_perawatan LIKE '%MRI%' OR jpr.nm_perawatan LIKE '%Magnetic Resonance%' THEN 'MR'
+    WHEN jpr.nm_perawatan LIKE '%NM%' OR jpr.nm_perawatan LIKE '%Nuklir%' OR jpr.nm_perawatan LIKE '%Nuclear Medicine%' THEN 'NM'
+    WHEN jpr.nm_perawatan LIKE '%OT%' OR jpr.nm_perawatan LIKE '%Other%' OR jpr.nm_perawatan LIKE '%Lain%' THEN 'OT'
+    WHEN jpr.nm_perawatan LIKE '%PT%' OR jpr.nm_perawatan LIKE '%PET%' OR jpr.nm_perawatan LIKE '%Positron%' THEN 'PT'
+    WHEN jpr.nm_perawatan LIKE '%SC%' OR jpr.nm_perawatan LIKE '%Scan Sekunder%' OR jpr.nm_perawatan LIKE '%Secondary Capture%' THEN 'SC'
+    WHEN jpr.nm_perawatan LIKE '%SR%' OR jpr.nm_perawatan LIKE '%Structured%' THEN 'SR'
+    WHEN jpr.nm_perawatan LIKE '%US%' OR jpr.nm_perawatan LIKE '%USG%' OR jpr.nm_perawatan LIKE '%Ultrasound%' THEN 'US'
+    WHEN jpr.nm_perawatan LIKE '%XA%' OR jpr.nm_perawatan LIKE '%Angio%' OR jpr.nm_perawatan LIKE '%Angiography%' THEN 'XA'
+    WHEN jpr.nm_perawatan LIKE '%XC%' OR jpr.nm_perawatan LIKE '%X-Ray Cine%' THEN 'XC'
+    ELSE 'OT'
     END AS Modality
 FROM
     permintaan_radiologi pr
@@ -86,6 +99,7 @@ WHERE
 	}
 	defer rows.Close()
 	var requests []WorklistRequest
+	var newReq []WorklistRequest
 	for rows.Next() {
 		var req WorklistRequest
 		err := rows.Scan(
@@ -107,7 +121,11 @@ WHERE
 		}
 		requests = append(requests, req)
 	}
-	return requests, nil
+	for _, v := range requests {
+		v.AccessionNumber = v.Modality + v.AccessionNumber + time.Now().Format("2006010215")
+		newReq = append(newReq, v)
+	}
+	return newReq, nil
 }
 
 func UpdateWorklistStatus(db *sql.DB, id int, status string) error {
@@ -120,8 +138,14 @@ func SaveStudyLinkToKhanza(db *sql.DB, noRawat string, link string) error {
 	return err
 }
 
-func SaveRadiologyResult(db *sql.DB, noRawat, tglPeriksa, jam, hasil string) error {
-	_, err := db.Exec(
+func SaveRadiologyResult(db *sql.DB, noorder, tglPeriksa, jam, hasil string) error {
+	var noRawatDB string
+	err := db.QueryRow("SELECT no_rawat FROM permintaan_radiologi WHERE noorder = ?", noorder).Scan(&noRawatDB)
+	if err != nil {
+		return err
+	}
+	noRawat := noRawatDB
+	_, err = db.Exec(
 		"INSERT INTO hasil_radiologi (no_rawat, tgl_periksa, jam, hasil) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE hasil=?, no_rawat=?",
 		noRawat, tglPeriksa, jam, hasil, hasil, noRawat,
 	)
--- main.go
+++ main.go
@@ -139,7 +139,7 @@ func processSRWebhook(cfg Config, db, mwdb *sql.DB, bodyBytes []byte) {
 	}
 	log.Printf("Hasil SR %s disimpan ke Khanza", payload.PatientID)
 	SavePortalLog(mwdb, "[SR] Hasil SR "+payload.PatientID+" disimpan ke Khanza")
-	UpdateHasilOrthanc(db, payload.PatientID, payload.StudyInstanceUID)
+	UpdateHasilOrthanc(db, payload.PatientID, string(hasilJSON))
 }
 
 func main() {
